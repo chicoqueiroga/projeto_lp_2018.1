a946b28b7f588d8432f5043f6bd1b79dd0e670d2
Francisco Queiroga <franciscofqueiroga@gmail.com>
2018-08-16 01:13:06 -0300

    Correções para segunda VA
    criação de tag referente aos últimos ajustes da segunda va

--- scripts/evolucoesProblemas/evolucoes_script.py
+++ scripts/evolucoesProblemas/evolucoes_script.py
@@ -4,29 +4,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from itertools import product
 
-def calculaComportamentoProblemasTrimestre(arquivo):
 
+def calculaComportamentoProblemasTrimestre(arquivo):
     caminhoCompleto = "../../../Data/Atendimentos Fornecedor/" + arquivo
 
     dadosTrimestre = pd.read_csv(caminhoCompleto, delimiter=';', na_filter=False, error_bad_lines=False)
 
     atendimentosRequeridos = ["Abertura Direta de Reclamação", "Reclamação de Ofício"]
 
-    dadosTrimestre = dadosTrimestre[(dadosTrimestre.DescricaoCNAEPrincipal != "NULL") & dadosTrimestre.DescricaoTipoAtendimento.isin(atendimentosRequeridos)]
+    dadosTrimestre = dadosTrimestre[
+        (dadosTrimestre.DescricaoCNAEPrincipal != "NULL") & dadosTrimestre.DescricaoTipoAtendimento.isin(
+            atendimentosRequeridos)]
     comportamentoProblemas = dadosTrimestre.GrupoProblema.value_counts()
 
     print("Fim do processamento do arquivo " + arquivo)
 
     return comportamentoProblemas
 
-def agrupaComportamentosProblemas(comportamentoAntes, comportamentoDepois):
 
+def agrupaComportamentosProblemas(comportamentoAntes, comportamentoDepois):
     evolucao = pd.concat([comportamentoAntes, comportamentoDepois], axis=1, sort=False)
 
     return evolucao
 
+
 anos = ["2014", "2015", "2016", "2017"]
-trimestres = ["1","2","3","4"]
+trimestres = ["1", "2", "3", "4"]
 combinacoes = product(anos, trimestres)
 
 arquivos = map(lambda (ano, numTrimestre): ano + "/Trimestre " + numTrimestre + ".csv", combinacoes)
@@ -36,10 +39,10 @@ evolucaoCompleta = reduce(agrupaComportamentosProblemas, evolucoesTrimestresSepa
 
 evolucaoCompleta = evolucaoCompleta.dropna()
 evolucaoCompleta = evolucaoCompleta.transpose()
-evolucaoCompleta.index = pd.date_range('01/01/2014', freq = '3MS', periods=16)
+evolucaoCompleta.index = pd.date_range('01/01/2014', freq='3MS', periods=16)
 
 print(evolucaoCompleta)
 
 evolucaoCompleta.plot()
-#eixos.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# eixos.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
--- scripts/grupoProdutosRepresentacao/grupoProdutosRepresentacao.py
+++ scripts/grupoProdutosRepresentacao/grupoProdutosRepresentacao.py
@@ -4,116 +4,121 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Cursor
 import sys
+
 sys.path.insert(0, '../utils/')
 from utils import pegaDfProcon
 
+
 def analiseDePareto(coluna, ano, df):
-	dfAnalisado = pd.Series.to_frame(df[coluna].value_counts())
-	dfAnalisado.reset_index(inplace=True)
-	dfAnalisado.columns = [coluna, 'qtdOcorrencia']
-	dfAnalisado.sort_values('qtdOcorrencia')
-	dfAnalisado['porcentagem'] = (dfAnalisado['qtdOcorrencia'] / dfAnalisado['qtdOcorrencia'].sum()) * 100
-	dfAnalisado['porcentagemAcumulada'] = dfAnalisado['porcentagem'].cumsum()
+    dfAnalisado = pd.Series.to_frame(df[coluna].value_counts())
+    dfAnalisado.reset_index(inplace=True)
+    dfAnalisado.columns = [coluna, 'qtdOcorrencia']
+    dfAnalisado.sort_values('qtdOcorrencia')
+    dfAnalisado['porcentagem'] = (dfAnalisado['qtdOcorrencia'] / dfAnalisado['qtdOcorrencia'].sum()) * 100
+    dfAnalisado['porcentagemAcumulada'] = dfAnalisado['porcentagem'].cumsum()
 
-	outrosDf = dfAnalisado[dfAnalisado.porcentagemAcumulada > 80]
-	dfAnalisado = dfAnalisado[dfAnalisado.porcentagemAcumulada <= 80]
-	dfAnalisado = dfAnalisado[[coluna, 'qtdOcorrencia', 'porcentagemAcumulada']]
-	dfAnalisado = dfAnalisado.append({coluna: 'Outros', 'qtdOcorrencia': outrosDf.qtdOcorrencia.sum(), 'porcentagemAcumulada': 100}, ignore_index=True)
-	dfAnalisado.set_index(coluna, inplace=True)
+    outrosDf = dfAnalisado[dfAnalisado.porcentagemAcumulada > 80]
+    dfAnalisado = dfAnalisado[dfAnalisado.porcentagemAcumulada <= 80]
+    dfAnalisado = dfAnalisado[[coluna, 'qtdOcorrencia', 'porcentagemAcumulada']]
+    dfAnalisado = dfAnalisado.append(
+        {coluna: 'Outros', 'qtdOcorrencia': outrosDf.qtdOcorrencia.sum(), 'porcentagemAcumulada': 100},
+        ignore_index=True)
+    dfAnalisado.set_index(coluna, inplace=True)
 
-	barras = dfAnalisado.qtdOcorrencia.plot(kind='bar', color='r')
-	linha  = dfAnalisado.porcentagemAcumulada.plot(linestyle='-', marker='o', ax=barras, secondary_y=True, color = '#008000', rot=90)
-	linha.set_yticks(np.arange(0,101,10))
-	
-	plt.savefig(ano + '/' + coluna + ' Pareto.png', bbox_inches='tight')
-	plt.show()
-	return list(dfAnalisado.index.values)
+    barras = dfAnalisado.qtdOcorrencia.plot(kind='bar', color='r')
+    linha = dfAnalisado.porcentagemAcumulada.plot(linestyle='-', marker='o', ax=barras, secondary_y=True,
+                                                  color='#008000', rot=90)
+    linha.set_yticks(np.arange(0, 101, 10))
+
+    plt.savefig(ano + '/' + coluna + ' Pareto.png', bbox_inches='tight')
+    plt.show()
+    return list(dfAnalisado.index.values)
 
 
 def representacaoAssuntoProblema(df):
-	ano = str(df['AnoAtendimento'].values[0])
-	
-	principaisProdutosServicos = analiseDePareto('GrupoAssunto', ano, df)
-	principaisProblemas = analiseDePareto('GrupoProblema', ano, df)
+    ano = str(df['AnoAtendimento'].values[0])
 
-	agrupamento = df[df.GrupoAssunto.isin(principaisProdutosServicos)].groupby('GrupoAssunto')['GrupoProblema'].value_counts()
+    principaisProdutosServicos = analiseDePareto('GrupoAssunto', ano, df)
+    principaisProblemas = analiseDePareto('GrupoProblema', ano, df)
 
-	outrosProblemas = agrupamento[~agrupamento.index.get_level_values('GrupoProblema').isin(principaisProblemas)]
-	outrosProblemas = outrosProblemas.unstack(level=-1, fill_value=0)
+    agrupamento = df[df.GrupoAssunto.isin(principaisProdutosServicos)].groupby('GrupoAssunto')[
+        'GrupoProblema'].value_counts()
 
-	outrosProblemas['Outros Problemas'] = outrosProblemas.sum(axis=1)
+    outrosProblemas = agrupamento[~agrupamento.index.get_level_values('GrupoProblema').isin(principaisProblemas)]
+    outrosProblemas = outrosProblemas.unstack(level=-1, fill_value=0)
 
-	outrosProblemas = pd.Series.to_frame(outrosProblemas['Outros Problemas'])
-	outrosProblemas = outrosProblemas.stack()
+    outrosProblemas['Outros Problemas'] = outrosProblemas.sum(axis=1)
 
-	agrupamento = agrupamento[agrupamento.index.get_level_values('GrupoProblema').isin(principaisProblemas)]
-	agrupamento = pd.concat([agrupamento, outrosProblemas])
+    outrosProblemas = pd.Series.to_frame(outrosProblemas['Outros Problemas'])
+    outrosProblemas = outrosProblemas.stack()
 
-	agrupamento = agrupamento.unstack(level=-1, fill_value=0)
-	agrupamento['totalOcorrencias'] = agrupamento.sum(axis=1)
+    agrupamento = agrupamento[agrupamento.index.get_level_values('GrupoProblema').isin(principaisProblemas)]
+    agrupamento = pd.concat([agrupamento, outrosProblemas])
 
-	agrupamento.sort_values('totalOcorrencias', inplace=True)
-	agrupamento.drop(columns = ['totalOcorrencias'], inplace=True)
+    agrupamento = agrupamento.unstack(level=-1, fill_value=0)
+    agrupamento['totalOcorrencias'] = agrupamento.sum(axis=1)
 
-	agrupamento.plot.barh(stacked=True, legend=True)
+    agrupamento.sort_values('totalOcorrencias', inplace=True)
+    agrupamento.drop(columns=['totalOcorrencias'], inplace=True)
 
-	ax = plt.gca()
-	myCursor = Cursor(ax, useblit=True, linewidth=2)
+    agrupamento.plot.barh(stacked=True, legend=True)
 
-	plt.savefig(ano + '/representacaoAssuntoProblema.png', bbox_inches='tight')
-	plt.show()
+    ax = plt.gca()
+    myCursor = Cursor(ax, useblit=True, linewidth=2)
 
-	return agrupamento
+    plt.savefig(ano + '/representacaoAssuntoProblema.png', bbox_inches='tight')
+    plt.show()
+
+    return agrupamento
 
 
 def cascata(antesDepois):
-	totalAntes = antesDepois[0]['Vício ou Má Qualidade'].sum()
-	totalDepois = antesDepois[1]['Vício ou Má Qualidade'].sum()
+    totalAntes = antesDepois[0]['Vício ou Má Qualidade'].sum()
+    totalDepois = antesDepois[1]['Vício ou Má Qualidade'].sum()
 
-	antes = pd.Series.to_frame(antesDepois[0]['Vício ou Má Qualidade'].tail(10))
-	depois = pd.Series.to_frame(antesDepois[1]['Vício ou Má Qualidade'])
+    antes = pd.Series.to_frame(antesDepois[0]['Vício ou Má Qualidade'].tail(10))
+    depois = pd.Series.to_frame(antesDepois[1]['Vício ou Má Qualidade'])
 
-	antes.columns = ['qtdAnterior']
+    antes.columns = ['qtdAnterior']
 
-	antesDepois = antes.join(depois, how='inner')
-	antesDepois.rename(columns={'Vício ou Má Qualidade': 'qtdPosterior'}, inplace=True)
-	antesDepois['diferenca'] = antesDepois.qtdPosterior - antesDepois.qtdAnterior
+    antesDepois = antes.join(depois, how='inner')
+    antesDepois.rename(columns={'Vício ou Má Qualidade': 'qtdPosterior'}, inplace=True)
+    antesDepois['diferenca'] = antesDepois.qtdPosterior - antesDepois.qtdAnterior
 
-	antesDepois = antesDepois['diferenca']
-	totalVariacoes = antesDepois.sum()
-	variacaoOutros = totalDepois - (totalAntes + totalVariacoes)
+    antesDepois = antesDepois['diferenca']
+    totalVariacoes = antesDepois.sum()
+    variacaoOutros = totalDepois - (totalAntes + totalVariacoes)
 
-	antesDepois['Outros'] = variacaoOutros
-	antesDepois = pd.concat([pd.Series(totalAntes, index=['totalAntes']), antesDepois])
+    antesDepois['Outros'] = variacaoOutros
+    antesDepois = pd.concat([pd.Series(totalAntes, index=['totalAntes']), antesDepois])
 
-	print(type(antesDepois))
-	print(antesDepois)
+    barraInvisivel = antesDepois.cumsum().shift(1).fillna(0)
+    barraInvisivel['totalDepois'] = totalDepois
+    antesDepois['totalDepois'] = totalDepois
 
-	barraInvisivel = antesDepois.cumsum().shift(1).fillna(0)
-	barraInvisivel['totalDepois'] = totalDepois
-	antesDepois['totalDepois'] = totalDepois
+    passo = barraInvisivel.reset_index(drop=True).repeat(3).shift(-1)
+    passo[1::3] = np.nan
 
-	passo = barraInvisivel.reset_index(drop=True).repeat(3).shift(-1)
-	passo[1::3] = np.nan
+    barraInvisivel['totalDepois'] = 0
 
-	barraInvisivel['totalDepois'] = 0
+    my_plot = antesDepois.plot(kind='bar', stacked=True, bottom=barraInvisivel, legend=None)
+    my_plot.plot(passo.index, passo.values, 'k')
 
-	my_plot = antesDepois.plot(kind='bar', stacked=True, bottom=barraInvisivel,legend=None)
-	my_plot.plot(passo.index, passo.values,'k')
+    ax = plt.gca()
 
-	ax = plt.gca()
+    ax.set_ylim(30000, 40000)
 
-	ax.set_ylim(30000, 40000)
+    plt.savefig('cascata2016-2017.png', bbox_inches='tight')
+    plt.tight_layout()
+    plt.show()
 
-	plt.savefig('cascata2016-2017.png', bbox_inches='tight')
-	plt.tight_layout()
-	plt.show()
 
 anos = ["2014", "2015", "2016", "2017"]
 
 myDfs = map(pegaDfProcon, anos)
 
 agrupamentos = map(representacaoAssuntoProblema, myDfs)
-agrupamentos = [(agrupamentos[0], agrupamentos[1]), (agrupamentos[1], agrupamentos[2]), (agrupamentos[2], agrupamentos[3])]
+agrupamentos = [(agrupamentos[0], agrupamentos[1]), (agrupamentos[1], agrupamentos[2]),
+                (agrupamentos[2], agrupamentos[3])]
 
 map(cascata, agrupamentos)
